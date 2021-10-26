4ef85f96476b80e41e6f49acb1330ba3259f2d3c
julandrod <julian.andres.rodriguez@gmail.com>
2021-10-26 10:11:28 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import madnessElectronics from "./img/screencapture-madnesselectronics.png"
+import madnessStore from "./img/screencapture-madness-store-netlify-app.png"
+
+export const products = [
+  {
+    id: 1,
+    img: madnessElectronics,
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: madnessStore,
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://www.uidesigndaily.com/uploads/1178/day_1178.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://www.uidesigndaily.com/uploads/1134/day_1134.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://www.uidesigndaily.com/uploads/903/day_903.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://www.uidesigndaily.com/uploads/160/day_160.jpg",
+    link: "http://lama.dev",
+  },
+];
